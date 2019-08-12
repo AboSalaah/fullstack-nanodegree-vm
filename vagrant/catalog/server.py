b4932f988e36ba5614c31ce4c8392aa76eda790b
AboSalaah <abosalaah96@gmail.com>
2019-08-12 11:03:21 +0200

    feat: Add a user to the database when not exist before

--- vagrant/catalog/server.py
+++ vagrant/catalog/server.py
@@ -150,6 +150,14 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
+
+    # if a user doesn't exist, make an entry for it
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id']=user_id
+
+    
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
