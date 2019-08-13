38a390ccfb61db3e52c7105d36acef43f65867d0
AboSalaah <abosalaah96@gmail.com>
2019-08-13 06:28:46 +0200

    feat: Add serialize functions to tables classes

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -24,6 +24,16 @@ class Category(Base):
     name = Column(String(250), nullable=False)
 
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+    
+
+
 
 class Item(Base):
     __tablename__ = 'items'
@@ -36,6 +46,19 @@ class Item(Base):
     category = relationship(Category)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'last_modification': self.last_modification,
+            'category_id': self.category_id,
+            'user_id': self.user_id
+        }
     
 
 
